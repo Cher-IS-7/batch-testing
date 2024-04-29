e87ff554643966909e0179515c04f25200310e75
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-29 22:33:41 +0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM Set the URL of the file you want to download
-set "url=https://raw.githubusercontent.com/Cher-IS-7/batch-testing/main/res-dwn.bat"
+set "url=hhttps://raw.githubusercontent.com/Cher-IS-7/batch-testing/main/main.bat"
 
 REM Set the path where you want to save the downloaded file
-set "output=%CD%\res-dwn.bat"
+set "output=%CD%\res-main.bat"
 
 REM Download the file using certutil
 certutil -urlcache -split -f "%url%" "%output%"
@@ -12,7 +12,7 @@ certutil -urlcache -split -f "%url%" "%output%"
 REM Notify the user that the download is complete
 echo File downloaded successfully.
 
-set "targetFile=res-dwn.bat"
+set "targetFile=res-main.bat"
 
 for /r C:\ %%f in (%targetFile%) do (
     if exist "%%f" (
