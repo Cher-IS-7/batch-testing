074318ecec1f0732ab778637eac96c2773293e98
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-29 22:19:24 +0400

    Update folder-dwnld.bat

--- folder-dwnld.bat
+++ folder-dwnld.bat
@@ -1,2 +1,2 @@
 @echo off
-svn export https://github.com/Cher-IS-7/batch-testing/tree/ce359e97bf5c4ead5c83d1405d2d7384f10acbfe/download
+set "url=https://github.com/Cher-IS-7/batch-testing/tree/ce359e97bf5c4ead5c83d1405d2d7384f10acbfe/download"
