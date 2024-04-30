7360cfca375c22c302581de4415c8d4526afb1d1
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-30 09:11:56 +0400

    Update res-dwn.bat

--- res-dwn.bat
+++ res-dwn.bat
@@ -2,5 +2,7 @@
 echo Res Downloader
 curl -OJL https://raw.githubusercontent.com/Cher-IS-7/batch-testing/main/download/permission.bat
 
+curl -OJL https://raw.githubusercontent.com/Cher-IS-7/batch-testing/main/run.bat
+
 
 exit
