c44de2dc15e7ed93985d1b3478f078b9e0544641
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-30 09:10:04 +0400

    Update res-dwn.bat

--- res-dwn.bat
+++ res-dwn.bat
@@ -1,10 +1,6 @@
 @echo off
 echo Res Downloader
-set "username=Cher-IS-7"
-set "repository=batch-testing"
-set "branch=b9826b75f57ea378ce394e678490b7d2a86cc6ae"
-set "folder=download"
+curl -OJL https://raw.githubusercontent.com/Cher-IS-7/batch-testing/main/download/permission.bat
 
-curl -LJO https://api.github.com/repos/%username%/%repository%/zipball/%branch%/%folder%
 
 exit
