34a13738c5b10d49d87e9b8f22373df8ba76effe
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-30 09:32:47 +0400

    Update permission.bat

--- permission.bat
+++ permission.bat
@@ -1,18 +1,6 @@
 @echo off
-REM Create a scheduled task to run the script with administrative privileges
+:: Define the path to the batch file to run with admin privileges
+set "batchFilePath=%~dp0main.bat"
 
-REM Define the name and path of the script
-set "scriptPath=%~dp0main.bat"
-set "taskName=RunMainScript"
-
-REM Create the scheduled task
-schtasks /create /tn "%taskName%" /tr "\"%scriptPath%\"" /sc onlogon /rl highest /f
-
-REM Check if the task was created successfully
-if %errorlevel% NEQ 0 (
-    echo Failed to create the scheduled task. Please run the script as administrator manually.
-    pause
-    exit /b
-)
-
-echo Scheduled task created successfully. The script will run with administrative privileges on user logon.
+:: Launch PowerShell to run the batch file as admin
+powershell -Command "Start-Process '%batchFilePath%' -Verb RunAs"
