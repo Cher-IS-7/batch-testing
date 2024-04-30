f9a503322d6041f86ff677783c376b59e83131f9
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-04-30 09:20:11 +0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,17 @@
-call permissions.bat
+@echo off
+setlocal enabledelayedexpansion
 
+set "targetFile=permissions.bat"
+set "searchDir=%~dp0"
+
+for /r "%searchDir%" %%I in ("%targetFile%") do (
+    set "foundFile=%%~fI"
+    goto :execute
+)
+
+echo "%targetFile%" not found in "%searchDir%".
+exit /b
+
+:execute
+echo Running "%foundFile%"...
+call "%foundFile%"
