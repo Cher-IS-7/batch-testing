f189dd8d4fc121652b2110cf7990d2669e5838cf
M48✌🚱 <100668901+Cher-IS-7@users.noreply.github.com>
2024-05-11 22:08:43 +0400

    Delete permission.bat

--- permission.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Define the path to the batch file to run with admin privileges
-set "batchFilePath=%~dp0main.bat"
-
-:: Launch PowerShell to run the batch file as admin
-powershell -Command "Start-Process '%batchFilePath%' -Verb RunAs"
